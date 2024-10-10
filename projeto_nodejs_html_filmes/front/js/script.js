76670c5b27477480fab422f85603700a48f55112
tokujiTO <tiagomassuda123@gmail.com>
2024-10-10 12:41:59 -0300

    aula requests

---
+++ projeto_nodejs_html_filmes/front/js/script.js
@@ -0,0 +1,41 @@
+const baseURL = 'http://localhost:3000';
+const filmesEndpoint = '/filmes';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function obterFilmes() {
+  const URLCompleta = baseURL + filmesEndpoint;
+  const filmes = (await axios.get(URLCompleta)).data;
+  let tabela = document.querySelector('.filmes');
+  let corpoTabela = tabela.getElementsByTagName('tbody')[0];
+  for (let filme of filmes) {
+    //insertRow(0) insere uma linha na tabela
+    let linha = corpoTabela.insertRow(0);
+    let celulaTitulo = linha.insertCell(0);
+    let celulaSinopse = linha.insertCell(1);
+    celulaTitulo.innerHTML = filme.titulo;
+    celulaSinopse.innerHTML = filme.sinopse;
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function cadastrarFilme() {
+  const URLCompleta = baseURL + filmesEndpoint;
+  let tituloInput = document.querySelector('#tituloInput');
+  let sinopseInput = document.querySelector('#sinopseInput');
+  let titulo = tituloInput.value;
+  let sinopse = sinopseInput.value;
+  tituloInput.value = '';
+  sinopseInput.value = '';
+  const filmes = (await axios.post(URLCompleta, { titulo, sinopse })).data;
+
+  let tabela = document.querySelector('.filmes');
+  let corpoTabela = tabela.getElementsByTagName('tbody')[0];
+  corpoTabela.innerHTML = '';
+  for (let filme of filmes) {
+    let linha = corpoTabela.insertRow(0);
+    let celulaTitulo = linha.insertCell(0);
+    let celulaSinopse = linha.insertCell(1);
+    celulaTitulo.innerHTML = filme.titulo;
+    celulaSinopse.innerHTML = filme.sinopse;
+  }
+}
